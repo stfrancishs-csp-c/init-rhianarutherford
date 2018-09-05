ca8ad86d48521234fbd15bfdedec278f09829939
Rhiana Rutherford <19rutherford@stfrancishs.org>
2018-09-05 19:58:41 +0000

    init

---
+++ datatypes.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+printf("I am learning the %c programming language\n", 'C');
+printf("I have  just completed Chapter %d\n", 2);
+printf("I am %.1f percent ready to move on\n", 99.9);
+printf("to the next chaper!\n");
+return 0;
+}
